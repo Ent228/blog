63234a8a39321f736d978529fcc82da3650f082d
Ent228 <pavlo.dihalevych@oa.edu.ua>
2021-05-09 23:13:04 +0300

    first commit

---
+++ app/Http/Controllers/Api/Blog/PostsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use App\Http\Controllers\PostController;
+
+class PostsController extends PostController
+{
+    public function index()
+    {
+
+    }
+}
